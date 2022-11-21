67dd53cbd1856894f3458ce808ac6711a2b71405
sspzz <richiefilth@protonmail.com>
2022-11-21 14:48:48 +0100

    Initial setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,59 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import {
+  WagmiConfig,
+  createClient,
+  configureChains,
+  defaultChains,
+} from "wagmi";
+import type { AppProps } from "next/app";
+import { publicProvider } from "wagmi/providers/public";
+import { CoinbaseWalletConnector } from "wagmi/connectors/coinbaseWallet";
+import { InjectedConnector } from "wagmi/connectors/injected";
+import { MetaMaskConnector } from "wagmi/connectors/metaMask";
+import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
+
+const { chains, provider, webSocketProvider } = configureChains(defaultChains, [
+  // alchemyProvider({
+  //   apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY ?? "",
+  //   priority: 0,
+  //   weight: 1,
+  // }),
+  publicProvider({ priority: 1, weight: 2 }),
+]);
+
+// Set up client
+const client = createClient({
+  autoConnect: true,
+  connectors: [
+    new MetaMaskConnector({ chains }),
+    new CoinbaseWalletConnector({
+      chains,
+      options: {
+        appName: "Wizzmas",
+      },
+    }),
+    new WalletConnectConnector({
+      chains,
+      options: {
+        qrcode: true,
+      },
+    }),
+    new InjectedConnector({
+      chains,
+      options: {
+        name: "Injected",
+        shimDisconnect: true,
+      },
+    }),
+  ],
+  provider,
+  webSocketProvider,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig client={client}>
+      <Component {...pageProps} />
+    </WagmiConfig>
+  );
 }
