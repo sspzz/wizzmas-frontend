d637d54af68ca30d9b6b3807872146cdaa8d0c56
sspzz <richiefilth@protonmail.com>
2022-12-15 09:38:40 +0100

    TS error fix, always return a fragment

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,51 +3,51 @@ import { useEffect, useState } from "react";
 import { useAccount, useNetwork } from "wagmi";
 import ConnectButton from "./ConnectButton";
 import { NextPage } from "next";
-import { useRouter} from "next/router";
+import { useRouter } from "next/router";
 
 interface NavLink {
-    name: string;
-    path: string;
+  name: string;
+  path: string;
 }
 
 const navLinks: NavLink[] = [
-    { name: "Covers", path: "/covers" },
-    {
-        name: "Send Cards",
-        path: "/send",
-    },
-    {
-        name: "My Cards",
-        path: "/view",
-    },
-    {
-        name: "Gallery",
-        path: "/gallery",
-    }
+  { name: "Covers", path: "/covers" },
+  {
+    name: "Send Cards",
+    path: "/send",
+  },
+  {
+    name: "My Cards",
+    path: "/view",
+  },
+  {
+    name: "Gallery",
+    path: "/gallery",
+  },
 ];
 
 const NavItem = ({ item }: { item: NavLink }) => {
-    const router = useRouter();
+  const router = useRouter();
 
-    return (
-        <p>
-            <a href={item.path}>
-                {router.pathname === item.path ? `[ ${item.name} ]` : item.name}
-            </a>
-        </p>
-    );
+  return (
+    <p>
+      <a href={item.path}>
+        {router.pathname === item.path ? `[ ${item.name} ]` : item.name}
+      </a>
+    </p>
+  );
 };
 
 const Header: NextPage = () => {
-    const { chain } = useNetwork();
-    const { address } = useAccount();
-    const [domLoaded, setDomLoaded] = useState(false);
+  const { chain } = useNetwork();
+  const { address } = useAccount();
+  const [domLoaded, setDomLoaded] = useState(false);
 
-    useEffect(() => {
-        setDomLoaded(true);
-    }, []);
+  useEffect(() => {
+    setDomLoaded(true);
+  }, []);
 
-    /*const wrongNetwork =
+  /*const wrongNetwork =
         (address &&
             chain?.id != 5 &&
             process.env.NEXT_PUBLIC_ALCHEMY_NETWORK == "goerli") ||
@@ -55,58 +55,60 @@ const Header: NextPage = () => {
             process.env.NEXT_PUBLIC_ALCHEMY_NETWORK == "mainnet") ||
         (chain?.id != 31337 &&
             process.env.NEXT_PUBLIC_ALCHEMY_NETWORK == "mainnet");*/
-    
-    const wrongNetwork = false;
-        
-    if (domLoaded) {
-        return (
-            <Nav>
-                <Title>
-                    <a href="/">
-                        <h1>Wizz Cards</h1>
-                    </a>
-                    <Connect>
-                        <ConnectButton />
-                        {wrongNetwork && (
-                            <WrongNetwork>Wallet connected to wrong Network!</WrongNetwork>
-                        )}
-                    </Connect>
-                </Title>
-                <Menu>
-                    <HSplit>
-                        <Buttons>
-                            {navLinks.map((l, key) => (
-                                <NavItem item={l} key={key} />
-                            ))}
-                        </Buttons>
-                    </HSplit>
-                </Menu>
-            </Nav>
-        );
-    }
+
+  const wrongNetwork = false;
+
+  return (
+    <>
+      {domLoaded && (
+        <Nav>
+          <Title>
+            <a href="/">
+              <h1>Wizz Cards</h1>
+            </a>
+            <Connect>
+              <ConnectButton />
+              {wrongNetwork && (
+                <WrongNetwork>Wallet connected to wrong Network!</WrongNetwork>
+              )}
+            </Connect>
+          </Title>
+          <Menu>
+            <HSplit>
+              <Buttons>
+                {navLinks.map((l, key) => (
+                  <NavItem item={l} key={key} />
+                ))}
+              </Buttons>
+            </HSplit>
+          </Menu>
+        </Nav>
+      )}
+    </>
+  );
 };
 
 const Nav = styled.nav`
-    top: 0px;
-    min-height: 100px;
-    min-width: 250px;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    margin-bottom: 3em;
-    flex: none;
+  top: 0px;
+  min-height: 100px;
+  min-width: 250px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin-bottom: 3em;
+  flex: none;
 `;
 
 const Title = styled.div`
-    padding-left: 2em;
-    padding-right: 2em;
-    display: flex;
-    background: #111;
-    flex-direction: row;
-    justify-content: space-between;
-    flex-wrap: wrap;
-    align-items: center;
-`
+  padding-left: 2em;
+  padding-right: 2em;
+  display: flex;
+  background: #111;
+  flex-direction: row;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  align-items: center;
+`;
 
 const Connect = styled.div`
   display: flex;
