6fab0e170c241678a3f33f2649695eb2eb47a1d2
0xJiro <jiro@sushi.com>
2022-12-15 00:08:59 -0500

    fixed viewers so they display correct cards for sender and recipients

--- components/card/CardSentViewer.tsx
+++ components/card/CardSentViewer.tsx
@@ -20,7 +20,7 @@ const CardSentViewer = () => {
   })
 
   const renderItem = (item: any) => {
-    const dynamicUrl = `${process.env.VERCEL_URL ?? 'http://localhost:3000'}/api/card/dynamic/${item}`
+    const dynamicUrl = `${process.env.VERCEL_URL ?? 'http://localhost:3000'}/api/card/dynamic/${senderIds[item]}`
     const [card, loadCard] = useState<any | undefined>(undefined)
 
     useEffect(() => {
